73763990346a6ad0e5d5ac1cf433a33f2838dc8b
Ignacio Fassini <nachofassini@gmail.com>
2017-08-28 22:02:36 -0300

    5th lesson

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -13,6 +13,8 @@
                 </div>
             </div>
         </div>
+        <hr>
+
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 @foreach($thread->replies as $reply)
@@ -20,5 +22,23 @@
                 @endforeach
             </div>
         </div>
+        <hr>
+
+        @if(auth()->check())
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea placeholder="Leave your reply" name="body" class="form-control" rows="5"></textarea>
+                        </div>
+
+                        <button class="btn btn-default">Post</button>
+                    </form>
+                </div>
+            </div>
+            @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+        @endif
     </div>
 @endsection
